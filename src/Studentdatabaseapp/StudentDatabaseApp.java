a4faed46e1db2349acc645b70e17b96d6b2d1383
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-10-23 22:31:25 +0530

    Create Object Student
    Create Object Call Student1 in Main Class

--- src/Studentdatabaseapp/StudentDatabaseApp.java
+++ src/Studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,7 @@ package Studentdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
+		Student student1 = new Student();
 		
 		//ask how many student we want add
 		
