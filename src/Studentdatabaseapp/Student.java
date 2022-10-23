87ea9760810b5aea1fe93e0edcfeb5ef0aea99f1
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-10-23 22:27:31 +0530

    Add Constructor
    Ussing constructor Implement Input Methods Get user Name , Campus year , and print Them

--- src/Studentdatabaseapp/Student.java
+++ src/Studentdatabaseapp/Student.java
@@ -1,20 +1,35 @@
 package Studentdatabaseapp;
+import java.util.Scanner;
 
 public class Student {
 	
 	//Adding the attribute 
 	private String firstName;
 	private String LastName;
-	private String grade;
+	private int gradeyear;
 	private int StudentID;
 	private String courses;
-	private String tuitionbalance;
+	private String paymentbalance;
 	private int costofCourse = 20000;
 	
 	
 	
 	//constructor : prompt user to enter student name and year
-	
+	//(Constructor Same as the Class name)
+	public Student() {
+		Scanner Sc = new Scanner(System.in);
+		
+		System.out.println("Enter Student First Name : ");
+		this.firstName = Sc.next();
+		
+		System.out.println("Enter Student Last Name : ");
+		this.LastName = Sc.next();
+		
+		System.out.println("Enter Student Year (Ex. year1 = 1 / year2 = 2) : ");
+		this.gradeyear = Sc.nextInt();
+		
+		System.out.println(firstName+" "+LastName+" "+gradeyear+" ");
+	}
 	
 	
 	//generate a ID
